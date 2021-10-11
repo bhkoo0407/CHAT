20ba67194453597cb42b6cc0f2e56ca8cf7e85d3
bhkoo0407 <bhkoo0407@naver.com>
2021-10-11 21:38:14 +0900

    chat 1 done chat 2 done chat 3 done chat 4 done chat 5 done

--- Chat/src/main/java/com/psw/chating/config/WebSocketConfig.java
+++ Chat/src/main/java/com/psw/chating/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.psw.chating.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.psw.chating.handler.SocketHandler;
 
@@ -20,4 +22,13 @@ public class WebSocketConfig implements WebSocketConfigurer{
 		// 구현체를 등록하였던 url정보를 방번호에 따라 구분될 수 있도록 변경해줍니다.
 		registry.addHandler(socketHandler, "/chating/{roomNumber}");
 	}
+	
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+	ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+	container.setMaxTextMessageBufferSize(500000);
+	container.setMaxBinaryMessageBufferSize(500000);
+	return container;
+	}
+
 }
--- Chat/src/main/java/com/psw/chating/handler/SocketHandler.java
+++ Chat/src/main/java/com/psw/chating/handler/SocketHandler.java
@@ -1,6 +1,10 @@
 package com.psw.chating.handler;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +13,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -16,67 +21,113 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
-	// 상속받은 TextWebSocketHandler는 handleTextMessage를 실행시키며, 메시지 타입에따라 
-	// handleBinaryMessage또는 handleTextMessage가 실행됩니다.
 	
-	//HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); 
-	//웹소켓 세션을 담아둘 맵 
-	List<HashMap<String, Object>> rls = new ArrayList<>(); 
-	//웹소켓 세션을 담아둘 리스트 ---roomListSessions
-		
-	//메세지를 수신하면 실행됩니다.
+	List<HashMap<String, Object>> rls = new ArrayList<>(); //웹소켓 세션을 담아둘 리스트 ---roomListSessions
+	private static final String FILE_UPLOAD_PATH = "C:/test/websocket/";
+	static int fileUploadIdx = 0;
+	static String fileUploadSession = "";
+	
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+		System.out.println("텍스트");
 		//메시지 발송
-		String msg = message.getPayload();
-		JSONObject obj = jsonToObjectParser(msg);
+		String msg = message.getPayload(); //JSON형태의 String메시지를 받는다.
+		JSONObject obj = jsonToObjectParser(msg); //JSON데이터를 JSONObject로 파싱한다.
 		
-		String rN = (String) obj.get("roomNumber");
+		String rN = (String) obj.get("roomNumber"); //방의 번호를 받는다.
+		String msgType = (String) obj.get("type"); //메시지의 타입을 확인한다.
 		HashMap<String, Object> temp = new HashMap<String, Object>();
-		//현재의 방번호를 가져오고 방정보+세션정보를 관리하는 rls리스트 컬랙션에서 데이터를 조회한 후에 해당 Hashmap을 임시 
-		// 맵에 파싱하여 roomNumber의 키값을 제외한 모든 세션키값들을 웹소켓을 통해 메시지를 보내줍니다.
 		if(rls.size() > 0) {
 			for(int i=0; i<rls.size(); i++) {
-				//세션리스트의 저장된 방번호를 가져와서
-				String roomNumber = (String) rls.get(i).get("roomNumber"); 
-				//같은값의 방이 존재한다면
-				if(roomNumber.equals(rN)) {
-					//해당 방번호의 세션리스트의 존재하는 모든 object값을 가져온다.
-					temp = rls.get(i); 
+				String roomNumber = (String) rls.get(i).get("roomNumber"); //세션리스트의 저장된 방번호를 가져와서
+				if(roomNumber.equals(rN)) { //같은값의 방이 존재한다면
+					temp = rls.get(i); //해당 방번호의 세션리스트의 존재하는 모든 object값을 가져온다.
+					fileUploadIdx = i;
+					fileUploadSession = (String) obj.get("sessionId");
 					break;
 				}
 			}
-			
-			//해당 방의 세션들만 찾아서 메시지를 발송해준다.
-			for(String k : temp.keySet()) { 
-				//다만 방번호일 경우에는 건너뛴다.
-				if(k.equals("roomNumber")) { 
-					continue;
-				}
-				
-				WebSocketSession wss = (WebSocketSession) temp.get(k);
-				if(wss != null) {
-					try {
-						wss.sendMessage(new TextMessage(obj.toJSONString()));
-					} catch (IOException e) {
-						e.printStackTrace();
+			if(!msgType.equals("fileUpload")) { //메시지의 타입이 파일업로드가 아닐때만 전송한다.
+				//해당 방의 세션들만 찾아서 메시지를 발송해준다.
+				for(String k : temp.keySet()) { 
+					if(k.equals("roomNumber")) { //다만 방번호일 경우에는 건너뛴다.
+						continue;
+					}
+					
+					WebSocketSession wss = (WebSocketSession) temp.get(k);
+					if(wss != null) {
+						try {
+							wss.sendMessage(new TextMessage(obj.toJSONString()));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
 		}
 	}
-	//웹소켓 연결이 되면 동작합니다.
+	
+	@Override
+	public void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+		System.out.print("바이너리");
+		//바이너리 메시지 발송
+		ByteBuffer byteBuffer = message.getPayload();
+		String fileName = "temp.jpg";
+		File dir = new File(FILE_UPLOAD_PATH);
+		if(!dir.exists()) {
+			dir.mkdirs();
+		}
+		
+		File file = new File(FILE_UPLOAD_PATH, fileName);
+		FileOutputStream out = null;
+		FileChannel outChannel = null;
+		try {
+			byteBuffer.flip(); //byteBuffer를 읽기 위해 세팅
+			out = new FileOutputStream(file, true); //생성을 위해 OutputStream을 연다.
+			outChannel = out.getChannel(); //채널을 열고
+			byteBuffer.compact(); //파일을 복사한다.
+			outChannel.write(byteBuffer); //파일을 쓴다.
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(out != null) {
+					out.close();
+				}
+				if(outChannel != null) {
+					outChannel.close();
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		byteBuffer.position(0); //파일을 저장하면서 position값이 변경되었으므로 0으로 초기화한다.
+		//파일쓰기가 끝나면 이미지를 발송한다.
+		HashMap<String, Object> temp = rls.get(fileUploadIdx);
+		for(String k : temp.keySet()) {
+			if(k.equals("roomNumber")) {
+				continue;
+			}
+			WebSocketSession wss = (WebSocketSession) temp.get(k);
+			try {
+				wss.sendMessage(new BinaryMessage(byteBuffer)); //초기화된 버퍼를 발송한다.
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("소켓 연결");
 		//소켓 연결
 		super.afterConnectionEstablished(session);
 		boolean flag = false;
 		String url = session.getUri().toString();
-		System.out.println(url);
 		String roomNumber = url.split("/chating/")[1];
-		//세션을 저장할때 현재 접근한 방의 정보가 있는지 체크하고 존재하지 않으면 방의 번호를 입력 후 세션들을 담아주는 로직으로 변경
-		int idx = rls.size(); 
+		int idx = rls.size(); //방의 사이즈를 조사한다.
 		if(rls.size() > 0) {
 			for(int i=0; i<rls.size(); i++) {
 				String rN = (String) rls.get(i).get("roomNumber");
@@ -88,12 +139,10 @@ public class SocketHandler extends TextWebSocketHandler {
 			}
 		}
 		
-		//존재하는 방이라면 세션만 추가한다.
-		if(flag) { 
+		if(flag) { //존재하는 방이라면 세션만 추가한다.
 			HashMap<String, Object> map = rls.get(idx);
 			map.put(session.getId(), session);
-		//최초 생성하는 방이라면 방번호와 세션을 추가한다.
-		}else { 
+		}else { //최초 생성하는 방이라면 방번호와 세션을 추가한다.
 			HashMap<String, Object> map = new HashMap<String, Object>();
 			map.put("roomNumber", roomNumber);
 			map.put(session.getId(), session);
@@ -107,12 +156,11 @@ public class SocketHandler extends TextWebSocketHandler {
 		session.sendMessage(new TextMessage(obj.toJSONString()));
 	}
 	
-	//웹소켓 연결이 종료되면 동작합니다.
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("소켓 종료");
 		//소켓 종료
-		//소켓이 종료되면 list컬랙션을 순회하면서 해당 세션값들을 찾아서 지운다.
-		if(rls.size() > 0) { 
+		if(rls.size() > 0) { //소켓이 종료되면 해당 세션값들을 찾아서 지운다.
 			for(int i=0; i<rls.size(); i++) {
 				rls.get(i).remove(session.getId());
 			}
