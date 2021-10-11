6833743dcc935b66bee389246071f915cbf86bc2
bhkoo0407 <bhkoo0407@naver.com>
2021-10-11 20:34:15 +0900

    chat 1 done chat 2 done chat 3 done chat 4 done

--- Chat/src/main/java/com/psw/chating/handler/SocketHandler.java
+++ Chat/src/main/java/com/psw/chating/handler/SocketHandler.java
@@ -1,6 +1,9 @@
 package com.psw.chating.handler;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,34 +19,88 @@ public class SocketHandler extends TextWebSocketHandler {
 	// 상속받은 TextWebSocketHandler는 handleTextMessage를 실행시키며, 메시지 타입에따라 
 	// handleBinaryMessage또는 handleTextMessage가 실행됩니다.
 	
-	
-	HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
-	
+	//HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); 
+	//웹소켓 세션을 담아둘 맵 
+	List<HashMap<String, Object>> rls = new ArrayList<>(); 
+	//웹소켓 세션을 담아둘 리스트 ---roomListSessions
+		
 	//메세지를 수신하면 실행됩니다.
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		//메시지 발송
 		String msg = message.getPayload();
 		JSONObject obj = jsonToObjectParser(msg);
-		for(String key : sessionMap.keySet()) {
-			WebSocketSession wss = sessionMap.get(key);
-			try {
-				wss.sendMessage(new TextMessage(obj.toJSONString()));
-			}catch(Exception e) {
-				e.printStackTrace();
+		
+		String rN = (String) obj.get("roomNumber");
+		HashMap<String, Object> temp = new HashMap<String, Object>();
+		//현재의 방번호를 가져오고 방정보+세션정보를 관리하는 rls리스트 컬랙션에서 데이터를 조회한 후에 해당 Hashmap을 임시 
+		// 맵에 파싱하여 roomNumber의 키값을 제외한 모든 세션키값들을 웹소켓을 통해 메시지를 보내줍니다.
+		if(rls.size() > 0) {
+			for(int i=0; i<rls.size(); i++) {
+				//세션리스트의 저장된 방번호를 가져와서
+				String roomNumber = (String) rls.get(i).get("roomNumber"); 
+				//같은값의 방이 존재한다면
+				if(roomNumber.equals(rN)) {
+					//해당 방번호의 세션리스트의 존재하는 모든 object값을 가져온다.
+					temp = rls.get(i); 
+					break;
+				}
+			}
+			
+			//해당 방의 세션들만 찾아서 메시지를 발송해준다.
+			for(String k : temp.keySet()) { 
+				//다만 방번호일 경우에는 건너뛴다.
+				if(k.equals("roomNumber")) { 
+					continue;
+				}
+				
+				WebSocketSession wss = (WebSocketSession) temp.get(k);
+				if(wss != null) {
+					try {
+						wss.sendMessage(new TextMessage(obj.toJSONString()));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 	}
-	
 	//웹소켓 연결이 되면 동작합니다.
 	@SuppressWarnings("unchecked")
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		//소켓 연결
 		super.afterConnectionEstablished(session);
-		sessionMap.put(session.getId(), session);
-		// 생성된 세션을 저장하면 발신메시지의 타입은 getID라고 명시 후 생성된 세션 ID값을 클라이언트단으로 발송합니다.
-		// 클라이언트단(jsp)에서는 type값을 통해 메시지와 초기 설정값을 구분할 예정입니다.
+		boolean flag = false;
+		String url = session.getUri().toString();
+		System.out.println(url);
+		String roomNumber = url.split("/chating/")[1];
+		//세션을 저장할때 현재 접근한 방의 정보가 있는지 체크하고 존재하지 않으면 방의 번호를 입력 후 세션들을 담아주는 로직으로 변경
+		int idx = rls.size(); 
+		if(rls.size() > 0) {
+			for(int i=0; i<rls.size(); i++) {
+				String rN = (String) rls.get(i).get("roomNumber");
+				if(rN.equals(roomNumber)) {
+					flag = true;
+					idx = i;
+					break;
+				}
+			}
+		}
+		
+		//존재하는 방이라면 세션만 추가한다.
+		if(flag) { 
+			HashMap<String, Object> map = rls.get(idx);
+			map.put(session.getId(), session);
+		//최초 생성하는 방이라면 방번호와 세션을 추가한다.
+		}else { 
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("roomNumber", roomNumber);
+			map.put(session.getId(), session);
+			rls.add(map);
+		}
+		
+		//세션등록이 끝나면 발급받은 세션ID값의 메시지를 발송한다.
 		JSONObject obj = new JSONObject();
 		obj.put("type", "getId");
 		obj.put("sessionId", session.getId());
@@ -54,12 +111,15 @@ public class SocketHandler extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		//소켓 종료
-		sessionMap.remove(session.getId());
+		//소켓이 종료되면 list컬랙션을 순회하면서 해당 세션값들을 찾아서 지운다.
+		if(rls.size() > 0) { 
+			for(int i=0; i<rls.size(); i++) {
+				rls.get(i).remove(session.getId());
+			}
+		}
 		super.afterConnectionClosed(session, status);
 	}
 	
-	// 메시지 전송 시 JSON파싱을 위해 message.getPayload()를 통해 문자열을 만든 함수 jsonToObjectParser에 넣어서
-	// JSONObject값으로 받아서 강제 문자열 형태로 보내주는부분이 추가되었습니다.
 	private static JSONObject jsonToObjectParser(String jsonStr) {
 		JSONParser parser = new JSONParser();
 		JSONObject obj = null;
--- Chat/src/main/webapp/WEB-INF/jsp/chat.jsp
+++ Chat/src/main/webapp/WEB-INF/jsp/chat.jsp
@@ -51,7 +51,8 @@
 	var ws;
 
 	function wsOpen(){
-		ws = new WebSocket("ws://" + location.host + "/chating");
+		//웹소켓 전송시 현재 방의 번호를 넘겨서 보낸다.
+		ws = new WebSocket("ws://" + location.host + "/chating/"+$("#roomNumber").val());
 		wsEvt();
 	}
 		
@@ -64,21 +65,12 @@
 			//메시지를 받으면 동작
 			var msg = data.data;
 			if(msg != null && msg.trim() != ''){
-				// 서버에서도 데이터를 JSON형태로 전달해주기 떄문에 받은 데이터를 JSON.parse메소드를 활용하여 파싱합니다.
 				var d = JSON.parse(msg);
-				// 파싱한 객체의 type값을 확인하여 getId값이면 초기 설정된 값이므로 채팅창에 값을 입력하는게 아니라 추가한
-				// 태그 sessionId에 값을 세팅해줍니다.
-				// 이 id값은 소켓이 종료되기 전까지 자기자신을 구분할 수 있는 session값이 될 예정입니다.
 				if(d.type == "getId"){
 					var si = d.sessionId != null ? d.sessionId : "";
 					if(si != ''){
 						$("#sessionId").val(si); 
 					}
-				// type이 message인 경우엔 채팅이 발생한 경우입니다.
-				// 상대방과 자신을 구분하기 위해 여기서 sessionId값을 사용합니다.
-				// 최초 이름을 입력하고 연결되었을떄, 발급받은 session값을 비교하여 같다면 자기 자신이 발신한 메시지이므로
-				// 오른쪽으로 정렬하는 클래스를 처리하고 메시지를 출력합니다.
-				// 비교하여 같지 않다면 타인이 발신한 메시지 이므로 왼쪽으로 정령하는 클래스를 처리하고 메시지를 출력합니다.
 				}else if(d.type == "message"){
 					if(d.sessionId == $("#sessionId").val()){
 						$("#chating").append("<p class='me'>나 :" + d.msg + "</p>");	
@@ -114,21 +106,20 @@
 	function send() {
 		var option ={
 			type: "message",
+			roomNumber: $("#roomNumber").val(), // 방번호 구분을 위해 추가됨
 			sessionId : $("#sessionId").val(),
 			userName : $("#userName").val(),
 			msg : $("#chatting").val()
 		}
-		// send함수의 발송하기전에 단순 String데이터가 아닌 obj값으로 값을 세팅하고 JSON형태로 발신처리로 변경 되었습니다.
-		// 메시지를 보낼땐 type값을 message로 구분하여 발송합니다.
 		ws.send(JSON.stringify(option))
 		$('#chatting').val("");
 	}
 </script>
 <body>
 	<div id="container" class="container">
-		<h1>채팅</h1>
-		<!-- 현재 세션값을 저장해놓기 위해 sessionId가 ID인 input태그를 추가하였습니다. -->
+		<h1>${roomName}의 채팅방</h1>
 		<input type="hidden" id="sessionId" value="">
+		<input type="hidden" id="roomNumber" value="${roomNumber}">
 		
 		<div id="chating" class="chating">
 		</div>
